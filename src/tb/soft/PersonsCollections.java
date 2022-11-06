6d76f5943fd7a06493d8780bff2ff585490d3046
ceramic-vessel-developer <263973@student.pwr.edu.pl>
2022-11-06 17:24:52 +0100

    add Persons Collections

---
+++ src/tb/soft/PersonsCollections.java
@@ -0,0 +1,15 @@
+package tb.soft;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+public class PersonsCollections {
+    //kolekcje przechowujące obiekty typu Person
+    static TreeSet<Person> TreeSetPersons = new TreeSet<>();
+    static HashSet<Person> HashSetPersons = new HashSet<>();
+    static LinkedList<Person> LinkedListPersons = new LinkedList<>();
+    static ArrayList<Person> ArrayListPersons = new ArrayList<>();
+
+}
