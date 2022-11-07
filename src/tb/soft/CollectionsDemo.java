3a2cc95cd6d5da0f16e7aec966879b66fb3ecd2e
SentiWW <sentiww@gmail.com>
2022-11-07 01:03:39 +0100

    Added collections demo with gui

---
+++ src/tb/soft/CollectionsDemo.java
@@ -0,0 +1,14 @@
+package tb.soft;
+
+import java.util.*;
+
+public class CollectionsDemo {
+    public final Set<Integer> hashSet = new HashSet<Integer>();
+    public final Set<Integer> treeSet = new TreeSet<Integer>();
+
+    public final List<Integer> arrayList = new ArrayList<Integer>();
+    public final List<Integer> linkedList = new LinkedList<Integer>();
+
+    public final Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+    public final Map<Integer, Integer> treeMap = new TreeMap<Integer, Integer>();
+}
